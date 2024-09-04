143143dbc5010f6c6ad8c6aadff9d7f6566dbd3e
Chris Viqueira <chrisviqueira@gmail.com>
2024-09-03 21:42:04 -0400

    Adding custom motions
    1. Resetting the [p]roject [v]iew to get out to the explore quickly.
    2. Setting the floating error window to view errors that are off-screen.

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,11 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+
+vim.keymap.set('n', '<leader>e', function()
+  vim.diagnostic.open_float(nil, { focusable = true })
+end, { desc = 'See the [e]rrors and warnings' })
+
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex, { desc = '[p]roject [v]iew' })
+
 return {}
